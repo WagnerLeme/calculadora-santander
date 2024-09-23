0fe87adb0d3a4300ad216e9a391b2719b902a7cc
Wagner Leme <wagner.wesley.leme96@gmail.com>
2024-09-22 21:54:26 -0300

    feat: implementa função de divisão

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,22 @@ function subtracao(numero1: number, numero2: number): number {
     return numero1 - numero2;
 }
 
-function multiplicacao(a: number, b: number): number {
-    return a * b;
+function multiplicacao(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+function divisao(numero1: number, numero2: number): number {
+    if (numero2 === 0) {
+        throw new Error("Divisão por zero não é permitida");
+    }
+
+    return numero1 / numero2;
 }
   
 console.log(adicao(5, 3));
 console.log(subtracao(5, 3));
 console.log(multiplicacao(5, 3));
+console.log(divisao(5, 3));
+//console.log(divisao(5, 0));
 
 console.log("Calculadora Básica - Adição, Subtração, Multiplicação e Divisão");
